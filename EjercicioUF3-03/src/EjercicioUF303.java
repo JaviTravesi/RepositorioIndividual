8ad0a9dfeafda784632ff31315f4afb69337c454
JaviTravesi <45272687+JaviTravesi@users.noreply.github.com>
2018-12-08 17:20:22 +0100

    Primer commit a local. 08/12/18 17:19

---
+++ EjercicioUF3-03/src/EjercicioUF303.java
@@ -0,0 +1,12 @@
+
+public class EjercicioUF303 {
+	public static void main(String[] args) {
+		int Num1 = (int) ((Math.random()*10)+1);
+		// System.out.println("El número que ha salido es "+Num1);
+		if (Num1>5) {
+			System.out.println("Hoy va a ser un gran día");
+		}
+		else 
+			System.out.println("Hoy va a ser el mejor día del mundo");
+	}
+}
